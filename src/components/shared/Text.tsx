9dfda82e3fab9f28f9e6b9f4344d40051c62fdc8
dlwl98 <ff981113@hanyang.ac.kr>
2023-10-27 00:28:55 +0900

    feat: Text 공통 컴포넌트 구현

---
+++ src/components/shared/Text.tsx
@@ -0,0 +1,46 @@
+import { HTMLAttributes } from 'react';
+
+import { css } from '@emotion/react';
+
+import { LINE_HEIGHT } from '@styles/font';
+
+type Textprops = {
+  size?: number | string;
+  weight?: 300 | 500 | 700;
+  lineHeight?: string;
+  ellipsis?: number;
+} & HTMLAttributes<HTMLParagraphElement>;
+
+export const Text = ({
+  children,
+  size = 16,
+  weight = 500,
+  lineHeight = LINE_HEIGHT,
+  ellipsis,
+  ...props
+}: Textprops) => {
+  const stringifiedSize = typeof size === 'number' ? `${size / 16}rem` : size;
+  const lineClamp = ellipsis
+    ? css`
+        overflow: hidden;
+        display: -webkit-box;
+        -webkit-box-orient: vertical;
+        -webkit-line-clamp: ${ellipsis};
+      `
+    : css``;
+
+  return (
+    <p
+      css={css`
+        margin: 0;
+        font-size: ${stringifiedSize};
+        font-weight: ${weight};
+        line-height: ${lineHeight};
+        ${lineClamp}
+      `}
+      {...props}
+    >
+      {children}
+    </p>
+  );
+};
