590d47900837a1729da60fd6544e7d9d031cc1c4
1g2g <wonji126326@gmail.com>
2023-10-30 00:20:18 +0900

    feat: 수락 카드 그룹 추가

--- src/components/shared/AllowCard/AllowCard.style.ts
+++ src/components/shared/AllowCard/AllowCard.style.ts
@@ -14,3 +14,9 @@ export const FlexBox = styled.div`
   display: flex;
   gap: 0.31rem;
 `;
+
+export const AllowCardGroupContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 0.75rem;
+`;
