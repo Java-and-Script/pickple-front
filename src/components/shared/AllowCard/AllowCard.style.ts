7d2348881c3cf9b9127ac5b794681e37723d1fec
1g2g <wonji126326@gmail.com>
2023-10-30 00:19:07 +0900

    feat: 수락 카드 컴포넌트 구현

---
+++ src/components/shared/AllowCard/AllowCard.style.ts
@@ -0,0 +1,16 @@
+import styled from '@emotion/styled';
+
+export const AllowCardContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  background-color: white;
+  border-radius: 8px;
+  padding: 10px;
+`;
+
+export const FlexBox = styled.div`
+  display: flex;
+  gap: 0.31rem;
+`;
