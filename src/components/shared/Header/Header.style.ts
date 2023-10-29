d7e15f17cf3a1cf491a8e5f36f19aaea7ae5cecd
1eecan <busyx2modernsociety@gmail.com>
2023-10-29 19:56:33 +0900

    style: Header.style.ts 추가

---
+++ src/components/shared/Header/Header.style.ts
@@ -0,0 +1,127 @@
+import styled from '@emotion/styled';
+
+export const HeaderBackground = styled.div`
+  position: fixed;
+  top: 0;
+  z-index: 888;
+  height: 2.5rem;
+  max-width: 375px;
+  width: 100%;
+  background-color: #ffffff;
+`;
+
+export const HeaderContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  ${({ theme }) => theme.STYLES.FLEX_ALIGN_CENTER}
+  justify-content: space-between;
+`;
+
+export const LogoWrapper = styled.div`
+  width: 3.06rem;
+  height: 2.13;
+`;
+
+export const LogoIcon = styled.button`
+  width: 100%;
+  height: 100%;
+  padding: 0;
+  border: none;
+  background-color: #ffffff;
+  cursor: pointer;
+
+  img {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+export const BackwardWrapper = styled.div`
+  width: 1.5rem;
+  height: 1.5rem;
+`;
+
+export const BackwardIcon = styled.button`
+  width: 100%;
+  height: 100%;
+  padding: 0;
+  border: none;
+  background-color: #ffffff;
+  cursor: pointer;
+
+  img {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+export const Title = styled.div`
+  color: black;
+`;
+
+export const RightContainer = styled.div`
+  width: 5.5rem;
+  ${({ theme }) => theme.STYLES.FLEX_JUSTIFY_CENTER}
+  justify-content: space-between;
+
+  &.invincible {
+    visibility: hidden;
+  }
+`;
+
+export const SearchIconWrapper = styled.div`
+  width: 1.5rem;
+  height: 1.5rem;
+`;
+
+export const SearchIcon = styled.button`
+  width: 100%;
+  height: 100%;
+  padding: 0;
+  border: none;
+  background-color: #ffffff;
+  cursor: pointer;
+
+  img {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+export const BellIconWrapper = styled.div`
+  width: 1.5rem;
+  height: 1.5rem;
+`;
+
+export const BellIcon = styled.button`
+  width: 100%;
+  height: 100%;
+  padding: 0;
+  border: none;
+  background-color: #ffffff;
+  cursor: pointer;
+
+  img {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+export const ProfileIconWrapper = styled.div`
+  width: 1.5rem;
+  height: 1.5rem;
+`;
+
+export const ProfileIcon = styled.button`
+  width: 100%;
+  height: 100%;
+  padding: 0;
+  border: none;
+  background-color: #ffffff;
+  cursor: pointer;
+
+  img {
+    width: 100%;
+    height: 100%;
+  }
+`;
