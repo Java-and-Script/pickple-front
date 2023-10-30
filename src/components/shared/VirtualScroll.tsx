909cb2a7f002fc0bd9c53d94b443509617d83360
김민재 <kimminje7267@naver.com>
2023-10-30 11:42:25 +0900

    feat: #41 VirtualScroll 컴포넌트 구현

---
+++ src/components/shared/VirtualScroll.tsx
@@ -0,0 +1,75 @@
+import { useEffect, useRef, useState } from 'react';
+
+import styled from '@emotion/styled';
+
+const StyledVirtualScrollWrapper = styled.div`
+  width: 17.5rem;
+  height: 8rem;
+  overflow: auto;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`;
+
+const StyledList = styled.div`
+  list-style: none;
+  display: flex;
+  flex-direction: column;
+`;
+
+const StyledItem = styled.div`
+  ${({ theme }) => theme.STYLES.FLEX_CENTER};
+  height: 2.5rem;
+  font-weight: ${({ theme }) => theme.FONT_WEIGHT.LIGHT};
+  color: ${({ theme }) => theme.PALETTE.GRAY_700};
+  transition: background-color 0.3s;
+`;
+
+const StyledCenterItem = styled(StyledItem)`
+  background-color: rgba(200, 200, 200, 0.3);
+`;
+
+export const VirtualScroll = ({ list }: { list: string[] }) => {
+  const scrollList = [null, ...list, null];
+  const scrollRef = useRef<HTMLDivElement>(null);
+  const [centerIndex, setCenterIndex] = useState(0);
+
+  useEffect(() => {
+    if (scrollRef.current) {
+      const scrollContainer = scrollRef.current as HTMLDivElement;
+      const updateCenterItemWithRef = () => {
+        const containerHeight = scrollContainer.clientHeight;
+        const itemHeight = 2.5 * 16;
+        const scrollTop = scrollContainer.scrollTop;
+        const centerScroll = containerHeight / 2;
+        const centerItemIndex = Math.floor(
+          (scrollTop + centerScroll) / itemHeight
+        );
+        setCenterIndex(centerItemIndex);
+      };
+      scrollContainer.addEventListener('scroll', updateCenterItemWithRef);
+
+      updateCenterItemWithRef();
+
+      return () => {
+        scrollContainer.removeEventListener('scroll', updateCenterItemWithRef);
+      };
+    }
+  }, []);
+
+  return (
+    <StyledVirtualScrollWrapper ref={scrollRef}>
+      <StyledList>
+        {scrollList.map((item, index) => (
+          <div key={index}>
+            {index === centerIndex ? (
+              <StyledCenterItem>{item}</StyledCenterItem>
+            ) : (
+              <StyledItem>{item}</StyledItem>
+            )}
+          </div>
+        ))}
+      </StyledList>
+    </StyledVirtualScrollWrapper>
+  );
+};
