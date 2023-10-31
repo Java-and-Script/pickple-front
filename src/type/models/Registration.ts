e77be474f6433958b81ec84270aaf53819134f13
1g2g <wonji126326@gmail.com>
2023-10-31 18:07:20 +0900

    fix: registration 타입 변경

--- src/type/models/Registration.ts
+++ src/type/models/Registration.ts
@@ -1,12 +1,12 @@
 export type Registration = {
   accessToken: string;
-  refreshToken: string | null;
-  id: number | null;
+  refreshToken: null;
+  id: null;
   nickname: string;
   profileImageUrl: string;
   email: string;
   oauthId: number;
   oauthProvider: string;
-  addressDepth1: string | null;
-  addressDepth2: string | null;
+  addressDepth1: null;
+  addressDepth2: null;
 };
