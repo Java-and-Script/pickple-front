7e9900e18de2c40c1ad3505fd0196d7440450021
1g2g <wonji126326@gmail.com>
2023-10-31 22:12:56 +0900

    fix: login 페이지 layout 적용

--- src/pages/LoginPage/LoginPage.style.ts
+++ src/pages/LoginPage/LoginPage.style.ts
@@ -2,16 +2,32 @@ import styled from '@emotion/styled';
 
 import { Image } from '@components/shared/Image';
 
+export const Wrapper = styled.div`
+  ${({ theme }) => theme.STYLES.LAYOUT}
+  height: 100dvh;
+  background-color: ${({ theme }) => theme.PALETTE.GRAY_100};
+`;
+
 export const LoginContainer = styled.div`
-  ${({ theme }) => theme.STYLES.FLEX_CENTER};
+  height: 100%;
+  ${({ theme }) => theme.STYLES.FLEX_ALIGN_CENTER};
   flex-direction: column;
-  justify-content: space-evenly;
-  gap: 10px;
+  justify-content: center;
+  gap: 20px;
 `;
 
 export const MainImage = styled(Image)`
   border-radius: 50px;
   object-position: top;
+  max-width: 767px;
+`;
+
+export const LogoImage = styled(Image)`
+  max-width: 767px;
+`;
+
+export const KakaoLoginImage = styled(Image)`
+  margin-bottom: 20px;
 `;
 
 export const RemoveLayout = styled.div`
--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,32 +1,37 @@
 import { Header } from '@components/shared/Header';
-import { Image } from '@components/shared/Image';
 
 import { IMG_SRC } from '@consts/imageSource';
 
 import KAKAO_LOGIN_SRC from '@assets/kakao_login_large_wide.png';
 import LOGO_SRC from '@assets/pickple_logo.png';
 
-import { LoginContainer, MainImage, RemoveLayout } from './LoginPage.style';
+import {
+  KakaoLoginImage,
+  LoginContainer,
+  LogoImage,
+  MainImage,
+  Wrapper,
+} from './LoginPage.style';
 
 export const LoginPage = () => {
   return (
-    <RemoveLayout>
+    <Wrapper className="hi">
       <Header isLogo={false} title="로그인" isRightContainer={false} />
       <LoginContainer>
-        <Image src={LOGO_SRC} width="30%" height="auto" alt="pickle logo" />
+        <LogoImage src={LOGO_SRC} width="35%" height="auto" alt="pickle logo" />
         <MainImage
           src={IMG_SRC.LOGIN_MAIN}
           width="100%"
           height="55%"
           alt="login main banner"
         />
-        <Image
+        <KakaoLoginImage
           src={KAKAO_LOGIN_SRC}
           width="100%"
           height="auto"
           alt="kakao login"
         />
       </LoginContainer>
-    </RemoveLayout>
+    </Wrapper>
   );
 };
