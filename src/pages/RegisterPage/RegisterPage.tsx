6af95017099e54ebb92ed861fcb38dd7c355c19c
1g2g <87280835+1g2g@users.noreply.github.com>
2023-11-01 14:27:47 +0900

    로그인, 정보 입력 페이지 구현 (#60)
    * feat: 라우트 및 기본 세팅 추가
    
    * feat: login 페이지 ui
    
    * feat: register 페이지 라우트 및 기본 세팅
    
    * feat: largeRedOulinedButton 속성 추가
    
    * feat: 정보 입력 페이지 ui
    
    * fix: 페이지 내 레이아웃 및 간격 조정
    
    * fix: login 페이지 layout 적용
    
    * fix: register 페이지 layout 적용
    
    * fix: styled components 이름 변경
    
    * fix: position 상수 분리, type에 반영
    
    * chore: png 삭제
    
    * chore: position 없음 추가

---
+++ src/pages/RegisterPage/RegisterPage.tsx
@@ -0,0 +1,99 @@
+import { useState } from 'react';
+
+import { LogoImage } from '@pages/LoginPage/LoginPage.style';
+
+import { Button } from '@components/shared/Button';
+import { Header } from '@components/shared/Header';
+import { SelectBox } from '@components/shared/SelectBox';
+import { Text } from '@components/shared/Text';
+import {
+  ToggleButton,
+  useToggleButtons,
+} from '@components/shared/ToggleButton';
+
+import { theme } from '@styles/theme';
+
+import { SEOUL } from '@consts/location';
+import { POSITIONS } from '@consts/positions';
+
+import LOGO_SRC from '@assets/logoSvg.svg';
+
+import {
+  FieldContainer,
+  Main,
+  PositionButtonGroup,
+  RegisterWrapper,
+  ScrollBox,
+} from './RegisterPage.style';
+
+export const RegisterPage = () => {
+  const [selectedLocation, setSelectedLocation] = useState<string[]>();
+  const [selectedPosition, setSelectedPosition] = useState<string[]>();
+
+  const {
+    handleToggle: handleToggleLocation,
+    selectedItems: selectedLocations,
+  } = useToggleButtons({
+    onToggle: setSelectedLocation,
+    isMultipleSelect: false,
+  });
+
+  const {
+    handleToggle: handleTogglePosition,
+    selectedItems: selectedPositions,
+  } = useToggleButtons({
+    onToggle: setSelectedPosition,
+    isMultipleSelect: true,
+  });
+
+  return (
+    <RegisterWrapper>
+      <Header isLogo={false} title="정보 입력" isRightContainer={false} />
+      <Main>
+        <LogoImage src={LOGO_SRC} width="35%" height="auto" alt="pickle logo" />
+        <FieldContainer>
+          <Text size="1.2rem" weight={700}>
+            주 활동 지역
+          </Text>
+          <ScrollBox>
+            <SelectBox border="none">
+              {Object.values(SEOUL).map((location) => (
+                <ToggleButton
+                  key={location}
+                  value={location}
+                  isActive={selectedLocations.includes(location)}
+                  border="none"
+                  onToggle={handleToggleLocation}
+                />
+              ))}
+            </SelectBox>
+          </ScrollBox>
+        </FieldContainer>
+        <FieldContainer>
+          <Text size="1.2rem" weight={700}>
+            주 포지션
+          </Text>
+          <PositionButtonGroup>
+            {POSITIONS.map((position) => (
+              <ToggleButton
+                key={position}
+                value={position}
+                isActive={selectedPositions.includes(position)}
+                onToggle={handleTogglePosition}
+              />
+            ))}
+          </PositionButtonGroup>
+        </FieldContainer>
+        <FieldContainer>
+          <Button
+            text="입력 완료"
+            width="100%"
+            height="3.125rem"
+            {...theme.BUTTON_PROPS.LARGE_RED_OUTLINED_BUTTON_PROPS}
+            onClick={() => console.log(selectedLocation, selectedPosition)}
+          />
+        </FieldContainer>
+      </Main>
+    </RegisterWrapper>
+  );
+};
--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -2,6 +2,8 @@ import { createBrowserRouter } from 'react-router-dom';
 
 import { HomePage } from '@pages/HomePage';
 import { Layout } from '@pages/Layout';
+import { LoginPage } from '@pages/LoginPage';
+import { RegisterPage } from '@pages/RegisterPage';
 
 export const router = createBrowserRouter([
   {
@@ -9,8 +11,8 @@ export const router = createBrowserRouter([
     element: <Layout />,
     children: [
       { path: '', element: <HomePage /> },
-      { path: 'login', element: <h1>login</h1> },
-      { path: 'register', element: <h1>register</h1> },
+      { path: 'login', element: <LoginPage /> },
+      { path: 'register', element: <RegisterPage /> },
       { path: 'all-services', element: <h1>all-services</h1> },
       { path: 'players/:id', element: <h1>players</h1> },
       {
