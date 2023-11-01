33a9892dbe66d624fa214aa71cc63c20b70a9919
1g2g <wonji126326@gmail.com>
2023-11-01 22:38:45 +0900

    feat: msw handler
    Co-authored-by: Chan Lee <1eecan@users.noreply.github.com>

--- src/mocks/handlers/member.ts
+++ src/mocks/handlers/member.ts
@@ -1,1 +1,93 @@
-export const memberHandlers = [];
+import { HttpResponse, http } from 'msw';
+
+import * as DATA from '../data/member';
+
+export const memberHandlers = [
+  http.get('/auth/:oAuthProvider', () => {
+    console.log('register page');
+
+    return HttpResponse.json(DATA.MEMBERS, {
+      status: 200,
+    });
+  }),
+
+  http.get('/auth/login/:oAuthProvider?authCode=:authCode', (req) => {
+    const { memberId } = req.params;
+
+    console.log(memberId);
+
+    return HttpResponse.json(DATA.ACCESS_TOKEN, {
+      status: 201,
+    });
+  }),
+
+  http.post('/auth/refresh', () => {
+    console.log('AuthToken Refreshed!');
+
+    return HttpResponse.json(DATA.ACCESS_TOKEN, {
+      status: 201,
+    });
+  }),
+
+  http.post('/members', () => {
+    console.log('/members');
+
+    return HttpResponse.json(DATA.MEMBERS, {
+      status: 302,
+    });
+  }),
+
+  http.get('/members/:memberId', (req) => {
+    const { memberId } = req.params;
+
+    console.log('/members/:memberId');
+    console.log(memberId);
+
+    return HttpResponse.json(DATA.MEMBERS_MEMBERID, {
+      status: 200,
+    });
+  }),
+
+  http.get('/members/:memberId/confirmed-games', ({ params, cookies }) => {
+    const { memberId } = params;
+
+    if (cookies !== null) return HttpResponse.json({}, { status: 400 });
+
+    console.log('/members/:memberId/confirmed-games');
+    console.log(memberId);
+
+    return HttpResponse.json(DATA.MEMBERS_MEMBERID_CONFIRMED_GAMES, {
+      status: 200,
+    });
+  }),
+  http.get('/members/:memberId/created-games', (req) => {
+    const { memberId } = req.params;
+
+    console.log('/members/:memberId/created-games');
+    console.log(memberId);
+
+    return HttpResponse.json(DATA.MEMBERS_MEMBERID_CREATED_GAMES, {
+      status: 200,
+    });
+  }),
+  http.get('/members/:memberId/joined-crews', (req) => {
+    const { memberId } = req.params;
+
+    console.log('/members/:memberId/joined-crews');
+    console.log(memberId);
+
+    return HttpResponse.json(DATA.MEMBERS_MEMBERID_JOINED_CREWS, {
+      status: 200,
+    });
+  }),
+  http.get('/members/:memberId/created-crews', (req) => {
+    const { memberId } = req.params;
+
+    console.log('/members/:memberId/created-crews');
+    console.log(memberId);
+
+    return HttpResponse.json(DATA.MEMBERS_MEMBERID_CREATED_CREWS, {
+      status: 200,
+    });
+  }),
+];
