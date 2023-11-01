75ff0fd4add91a31cc5bc2b02e24746f3709a197
dlwl98 <ff981113@hanyang.ac.kr>
2023-11-02 01:04:08 +0900

    fix: getGameMembers 함수명 수정

--- src/api/games/getGameMembers.ts
+++ src/api/games/getGameMembers.ts
@@ -3,13 +3,15 @@ import { axiosInstance } from '@api/axiosInstance';
 import { GetGameMembersResponse } from '@type/api/games';
 import { Game } from '@type/models';
 
-export const getGameMebers = async ({
-  gameId,
-  status,
-}: {
+export type getGameMembersProps = {
   gameId: Game['id'];
   status: '대기' | '확정';
-}) => {
+};
+
+export const getGameMembers = async ({
+  gameId,
+  status,
+}: getGameMembersProps) => {
   const { data } = await axiosInstance.get<GetGameMembersResponse>(
     `/api/games/${gameId}/members`,
     {
