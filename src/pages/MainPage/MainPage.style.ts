cdc333e638701a2fc70f0a65193b470b52b8eb21
1eecan <busyx2modernsociety@gmail.com>
2023-11-01 11:37:32 +0900

    fix: layout 적용

--- src/pages/MainPage/MainPage.style.ts
+++ src/pages/MainPage/MainPage.style.ts
@@ -1,12 +1,10 @@
 import styled from '@emotion/styled';
 
-import { theme } from '@styles/theme';
-
 export const MainPageContainer = styled.div`
   width: 100vw;
-  padding-top: 0.6rem;
-  padding-bottom: 5rem;
-  background-color: ${theme.PALETTE.GRAY_100};
+  margin-top: 0.6rem;
+  ${({ theme }) => theme.STYLES.LAYOUT}
+  background-color: ${({ theme }) => theme.PALETTE.GRAY_100};
 `;
 
 export const MainPageSubContainer = styled.div`
@@ -14,6 +12,5 @@ export const MainPageSubContainer = styled.div`
   display: flex;
   flex-direction: column;
   gap: 10px;
-  padding: 0 10px 0 10px;
   margin-bottom: 1.25rem;
 `;
