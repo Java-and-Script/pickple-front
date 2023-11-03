e668c49a7818dce41d55a15a6cb9e361a5fcc66c
1g2g <wonji126326@gmail.com>
2023-11-03 15:38:57 +0900

    feat: all-services 페이지 ui

---
+++ src/pages/AllServicesPage/AllServicesPage.style.ts
@@ -0,0 +1,40 @@
+import styled from '@emotion/styled';
+
+export const AllServicesContainer = styled.div`
+  ${({ theme }) => theme.STYLES.LAYOUT}
+  min-height: 100dvh;
+  background-color: ${({ theme }) => theme.PALETTE.GRAY_100};
+`;
+
+export const Main = styled.div`
+  background-color: ${({ theme }) => theme.PALETTE.GRAY_100};
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  margin: 10px 0;
+`;
+
+export const FieldContainer = styled.div`
+  width: 100%;
+  display: flex;
+  gap: 10px;
+  flex-direction: column;
+`;
+
+export const Item = styled.div`
+  background-color: white;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  width: 100%;
+  height: 40px;
+  box-sizing: border-box;
+  padding: 4px;
+  border-radius: 8px;
+  svg {
+    path {
+      fill: ${({ theme }) => theme.PALETTE.GRAY_500};
+    }
+  }
+`;
--- src/pages/AllServicesPage/AllServicesPage.tsx
+++ src/pages/AllServicesPage/AllServicesPage.tsx
@@ -1,3 +1,82 @@
+import { ReactNode } from 'react';
+
+import { Header } from '@components/Header';
+import { Text } from '@components/shared/Text';
+
+import Ball from '@assets/ball.svg?react';
+import Chat from '@assets/chat.svg?react';
+import CrewManage from '@assets/crewManage.svg?react';
+import CrewMember from '@assets/crewMember.svg?react';
+import Exit from '@assets/exit.svg?react';
+import Map from '@assets/map.svg?react';
+import Medal from '@assets/medal.svg?react';
+import Pen from '@assets/pen.svg?react';
+import Profile from '@assets/profile.svg?react';
+import Social from '@assets/social.svg?react';
+import Whistle from '@assets/whistle.svg?react';
+
+import {
+  AllServicesContainer,
+  FieldContainer,
+  Item,
+  Main,
+} from './AllServicesPage.style';
+
+type MenuItemProps = {
+  icon: ReactNode;
+  pageName: string;
+};
+const MenuItem = ({ icon, pageName }: MenuItemProps) => {
+  return (
+    <Item>
+      {icon}
+      <Text size="1rem" weight={500}>
+        {pageName}
+      </Text>
+    </Item>
+  );
+};
 export const AllServicesPage = () => {
-  return <>전체 메뉴</>;
+  return (
+    <AllServicesContainer>
+      <Header isLogo={true} isRightContainer={true} />
+
+      <Main>
+        <Text size="1.2rem" weight={700}>
+          전체 서비스
+        </Text>
+        <FieldContainer>
+          <Text size="1rem" weight={700}>
+            메인
+          </Text>
+          <MenuItem icon={<Map />} pageName="지도" />
+          <MenuItem icon={<Pen />} pageName="글쓰기" />
+          <MenuItem icon={<Chat />} pageName="메세지" />
+          <MenuItem icon={<Profile />} pageName="내 정보" />
+          <MenuItem icon={<Social />} pageName="소셜링" />
+        </FieldContainer>
+        <FieldContainer>
+          <Text size="1rem" weight={700}>
+            경기
+          </Text>
+          <MenuItem icon={<Ball />} pageName="게스트로 참가한 경기 목록" />
+          <MenuItem icon={<Whistle />} pageName="내가 만든 경기 목록" />
+        </FieldContainer>
+        <FieldContainer>
+          <Text size="1rem" weight={700}>
+            크루
+          </Text>
+          <MenuItem icon={<CrewManage />} pageName="내가 속한 크루 목록" />
+          <MenuItem icon={<CrewMember />} pageName="내가 만든 크루 관리" />
+          <MenuItem icon={<Medal />} pageName="크루 랭킹" />
+        </FieldContainer>
+        <FieldContainer>
+          <Text size="1rem" weight={700}>
+            설정
+          </Text>
+          <MenuItem icon={<Exit />} pageName="로그아웃" />
+        </FieldContainer>
+      </Main>
+    </AllServicesContainer>
+  );
 };
