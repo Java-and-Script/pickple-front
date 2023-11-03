00c2a1332a4e2cb742302e1fa64cc4b3ad7adeeb
dlwl98 <ff981113@hanyang.ac.kr>
2023-11-03 17:13:04 +0900

    feat: GamesNearPage 나의 주 활동지역 가져오는 기능 추가

--- src/pages/GamesNearPage/GamesNearPage.tsx
+++ src/pages/GamesNearPage/GamesNearPage.tsx
@@ -2,18 +2,36 @@ import { Header } from '@components/Header';
 import { MatchItem } from '@components/MatchItem';
 import { Text } from '@components/shared/Text';
 
-import { useGamesQuery } from '@hooks/queries/useGamesQuery';
+import { GamesQueryProps, useGamesQuery } from '@hooks/queries/useGamesQuery';
 import { useHeaderTitle } from '@hooks/useHeaderTitle';
 import { useInfiniteScroll } from '@hooks/useInfiniteScroll';
+
+import { Member } from '@type/models';
 
 import { getGameStartDate } from '@utils/domain';
 
 import { PageContent, PageLayout } from './GamesNearPage.styles';
 
+const getMyInfo = () => {
+  const json = localStorage.getItem('USER_INFO');
+  if (!json) {
+    return null;
+  }
+  return JSON.parse(json) as Member;
+};
+
 /** TODO: 내 정보에서 주 활동지역을 가져와서 useGamesQuery에 넘겨줘야 함 */
 export const GamesNearPage = () => {
   const { entryRef, showHeaderTitle } = useHeaderTitle<HTMLDivElement>();
-  const { games, fetchNextPage } = useGamesQuery({});
+  const myInfo = getMyInfo();
+
+  const gamesQueryProps: GamesQueryProps = myInfo
+    ? {
+        category: 'location',
+        value: `${myInfo?.addressDepth1}+${myInfo?.addressDepth2}`,
+      }
+    : {};
+  const { games, fetchNextPage } = useGamesQuery(gamesQueryProps);
   const lastElementRef = useInfiniteScroll<HTMLDivElement>(fetchNextPage);
 
   return (
