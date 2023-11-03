e72ef26d4ddff04906a2c6a886b15309cd26167f
1g2g <wonji126326@gmail.com>
2023-11-03 18:38:43 +0900

    feat: 로그인 임시 로직

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,5 +1,7 @@
 import { Header } from '@components/Header';
 
+import { useLoginQuery } from '@hooks/queries/useLoginQuery';
+
 import KAKAO_LOGIN_SRC from '@assets/kakao_login_large_wide.png';
 import LOGO_SRC from '@assets/logoSvg.svg';
 
@@ -15,6 +17,20 @@ const LOGIN_MAIN =
   'https://github.com/Java-and-Script/pickple-front/assets/87280835/1134921d-2e91-4b47-b99a-4095c91f0a6d';
 
 export const LoginPage = () => {
+  const { refetch } = useLoginQuery({
+    oauthProvider: 'KAKAO',
+    authCode: '23',
+  });
+
+  const onClickKakaoLogin = async () => {
+    const { data } = await refetch();
+    if (!data) {
+      return;
+    }
+    localStorage.setItem('LOGIN_INFO', JSON.stringify(data));
+    localStorage.setItem('ACCESS_TOKEN', data.accessToken);
+  };
+
   return (
     <LoginContainer className="hi">
       <Header isLogo={false} title="로그인" isRightContainer={false} />
@@ -31,6 +47,7 @@ export const LoginPage = () => {
           width="100%"
           height="auto"
           alt="kakao login"
+          onClick={() => onClickKakaoLogin()}
         />
       </Main>
     </LoginContainer>
