98eabe0f495b8f7f90c36553e125daa898b3e003
dlwl98 <ff981113@hanyang.ac.kr>
2023-11-05 10:22:20 +0900

    fix: getConfirmedGames 리턴 타입 수정

--- src/api/member/getConfirmedGames.ts
+++ src/api/member/getConfirmedGames.ts
@@ -1,11 +1,14 @@
 import { axiosInstance } from '@api/axiosInstance';
 
-import { GetConfirmedGamesRequest } from '@type/api/member';
+import {
+  GetConfirmedGamesRequest,
+  GetConfirmedGamesResponse,
+} from '@type/api/member';
 
 export const getConfirmedGames = async ({
   memberId,
 }: GetConfirmedGamesRequest) => {
-  const { data } = await axiosInstance.get<GetConfirmedGamesRequest>(
+  const { data } = await axiosInstance.get<GetConfirmedGamesResponse>(
     `/members/${memberId}/confirmed-games`,
     {}
   );
