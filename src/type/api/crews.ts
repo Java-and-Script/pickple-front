2b39bf00359b6e06f9574ff355b792ea8f954fef
1g2g <87280835+1g2g@users.noreply.github.com>
2023-11-07 17:26:05 +0900

    feat: crew api 타입 정의 (#140)
    Co-authored-by: Chan Lee <1eecan@users.noreply.github.com>
    Co-authored-by: Minjae Kim <kimminje7267@naver.com>
    Co-authored-by: 이진욱 <ff981113@hanyang.ac.kr>

---
+++ src/type/api/crews.ts
@@ -0,0 +1,48 @@
+import type { Crew, CrewProfile, Member } from '@type/models';
+import type { Registration } from '@type/models';
+
+export type PostCrewRequest = Pick<
+  Crew,
+  'name' | 'content' | 'maxMemberCount' | 'addressDepth1' | 'addressDepth2'
+> & { leaderId: Member['id'] };
+
+export type PostCrewResponse = {
+  crewId: Crew['id'];
+};
+
+export type GetCrewDetailRequest = {
+  crewId: Crew['id'];
+};
+export type GetCrewDetailResponse = CrewProfile;
+
+export type PostCrewParticipateRequest = {
+  crewId: Crew['id'];
+  memberId: Member['id'];
+};
+
+export type GetCrewParticipateListRequest = {
+  crewId: Crew['id'];
+  status: '대기' | '확정';
+};
+
+export type GetCrewParticipateListResponse = CrewProfile;
+
+export type PatchCrewParticipateAllowRequest = {
+  crewId: Crew['id'];
+  memberId: Member['id'];
+  status: '확정';
+};
+
+export type GetNearCrewListRequest = {
+  addressDepth1: Registration['addressDepth1'];
+  addressDepth2: Registration['addressDepth2'];
+  page: number;
+  size: number;
+};
+
+export type GetNearCrewListResponse = CrewProfile[];
+
+export type DeleteCrewParticipateRequest = {
+  crewId: Crew['id'];
+  memberId: Member['id'];
+};
