01ec7d92c1da2ef39be6fe65f38752f7f7a1185c
1eecan <busyx2modernsociety@gmail.com>
2023-11-08 15:07:22 +0900

    feat: 크루 리스트 api 호출 함수 및 메인페이지용 쿼리작성
    * 크루 타입 일부 수정

---
+++ src/pages/MainPage/useMainPageNearCrewListQuery.ts
@@ -0,0 +1,24 @@
+import { useSuspenseQuery } from '@tanstack/react-query';
+
+import { getNearCrewList } from '@api/crews/getNearCrewList';
+
+const FETCH_SIZE = 3;
+
+export const useMainPageNearCrewListQuery = ({
+  addressDepth1,
+  addressDepth2,
+}: {
+  addressDepth1: string;
+  addressDepth2: string;
+}) => {
+  return useSuspenseQuery({
+    queryKey: ['mainpage-crews'],
+    queryFn: () =>
+      getNearCrewList({
+        addressDepth1,
+        addressDepth2,
+        page: 1,
+        size: FETCH_SIZE,
+      }),
+  });
+};
