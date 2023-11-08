8c194f9ecb4edcf89e5acc7c9a4a9f9b8637a859
이진욱 <ff981113@hanyang.ac.kr>
2023-11-08 18:09:43 +0900

    axiosInstance baseUrl 변경 (#158)
    * chore: axios baseURL 변경
    - axiosInstance.ts 변경
    - vite config proxy 변경
    
    * fix: GamesHostPage 리뷰남기기 버튼 클릭핸들러 변경
    
    * chore: vitest config proxy 변경
    
    * fix: post patch api 함수 수정
    - payload 잘 안담기던 것 수정
    
    * fix: formatDate 함수 수정
    
    * fix: AllServicePage 로컬스토리지 키 변경

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -2,8 +2,11 @@ import axios from 'axios';
 
 import { setAuthorization } from './interceptors';
 
+const PROD = import.meta.env.MODE === 'production';
+const baseURL = PROD ? import.meta.env.VITE_BASE_URL : '/api';
+
 export const axiosInstance = axios.create({
-  baseURL: '/api',
+  baseURL,
   headers: {
     'Content-type': 'application/json',
   },
