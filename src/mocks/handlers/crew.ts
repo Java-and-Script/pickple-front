b404245d7565e1808832b1b04c03a5eb75de67d3
dlwl98 <ff981113@hanyang.ac.kr>
2023-11-09 00:09:39 +0900

    feat: 크루 상세 mock api handler 작성

--- src/mocks/handlers/crew.ts
+++ src/mocks/handlers/crew.ts
@@ -1,1 +1,9 @@
-export const crewHandlers = [];
+import { HttpResponse, http } from 'msw';
+
+import { CREW_DETAIL } from '@mocks/data/crew';
+
+const mockGetCrewDetail = http.get('/api/crews/:crewId', () => {
+  return HttpResponse.json(CREW_DETAIL);
+});
+
+export const crewHandlers = [mockGetCrewDetail];
