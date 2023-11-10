a37aa824927c0cc693c5bd636e57861a55ce9d29
김민재 <kimminje7267@naver.com>
2023-11-10 16:59:40 +0900

    feat: TextArea 컴포넌트 구현

---
+++ src/components/TextArea/TextArea.styles.ts
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled';
+
+export const StyledSubTitle = styled.div`
+  margin-bottom: 8px;
+`;
+
+export const StyledTextArea = styled.textarea`
+  border: 1px solid ${({ theme }) => theme.PALETTE.GRAY_300};
+  border-radius: 8px;
+  width: 100%;
+  height: 340px;
+  padding: 8px;
+  margin-bottom: 16px;
+`;
