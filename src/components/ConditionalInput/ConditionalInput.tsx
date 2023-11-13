3e1be9d324e86e2e44b96ba2a3d057bb186842f7
Minjae Kim <kimminje7267@naver.com>
2023-11-13 15:34:46 +0900

    게스트 모집글 생성 페이지 리팩토링 (#174)
    * fix: PostGameRequest Type 멤버 hostId 제거
    
    * fix: 게스트 모집글 생성 페이지 api 연동 수정
    
    - 로그인 안되어 있을 때 접근 차단
    - 폼 데이터 제출 성공시 생성된 게스트 모집글 페이지로 이동
    
    * fix: 게스트 모집 생성 페이지 api Mocking handler 수정
    
    * feat: ConditionalInput 컴포넌트 구현
    
    * feat: TextArea 컴포넌트 구현
    
    * feat: createGameOptions 상수화 분리
    
    * refactor: 게스트 모집글 작성 페이지 수정

---
+++ src/components/ConditionalInput/ConditionalInput.tsx
@@ -0,0 +1,86 @@
+import { useForm } from 'react-hook-form';
+
+import { Modal } from '@components/Modal';
+import { Text } from '@components/shared/Text';
+
+import {
+  StyledInput,
+  StyledModalHeader,
+  StyledSubTitle,
+} from './ConditionalInput.styles';
+
+type ConditionalInputProps = {
+  title: string;
+  min?: string;
+  max?: string;
+  step?: number;
+  required?: boolean;
+  readOnly?: boolean;
+  inputType?: string;
+  inputLabel: string;
+  inputValue?: string;
+  isModalOpen?: boolean;
+  inputPattern?: string;
+  isContainModal: boolean;
+  children?: React.ReactNode;
+  inputOnClick?: () => void;
+  inputOnChange?: (item: string) => void;
+  closeModal?: () => void;
+};
+
+export const ConditionalInput = ({
+  min,
+  max,
+  step,
+  title,
+  inputType,
+  readOnly = false,
+  isContainModal = false,
+  inputLabel,
+  inputOnClick,
+  inputOnChange,
+  inputValue,
+  isModalOpen,
+  closeModal,
+  children,
+  inputPattern,
+  required = true,
+}: ConditionalInputProps) => {
+  const { register } = useForm();
+  return (
+    <>
+      <StyledSubTitle>
+        <Text size={16} weight={300}>
+          {title}
+        </Text>
+      </StyledSubTitle>
+      <StyledInput
+        {...register(inputLabel)}
+        required={required}
+        type={inputType}
+        min={min}
+        max={max}
+        step={step}
+        pattern={inputPattern}
+        onChange={(event) => inputOnChange && inputOnChange(event.target.value)}
+        readOnly={readOnly}
+        onClick={inputOnClick}
+        value={inputValue}
+      />
+      {isContainModal && (
+        <Modal
+          isOpen={isModalOpen as boolean}
+          close={closeModal as VoidFunction}
+          header={true}
+        >
+          <StyledModalHeader>
+            <Text size={20} weight={700}>
+              {title}
+            </Text>
+          </StyledModalHeader>
+          <Modal.Content>{children}</Modal.Content>
+        </Modal>
+      )}
+    </>
+  );
+};
