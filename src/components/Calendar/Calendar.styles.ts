26c3ff9148c99c4d40d2296ad354ee3367902172
김민재 <kimminje7267@naver.com>
2023-11-14 02:54:36 +0900

    style: Calendar 컴포넌트 스타일 수정

--- src/components/Calendar/Calendar.styles.ts
+++ src/components/Calendar/Calendar.styles.ts
@@ -3,10 +3,44 @@ import Calendar from 'react-calendar';
 import styled from '@emotion/styled';
 
 export const StyledCalendar = styled(Calendar)`
-  abbr {
-    text-decoration: none;
-  }
   width: 100dvw;
   padding: 20px;
   border: none;
+  abbr {
+    text-decoration: none;
+  }
+  .react-calendar__navigation button {
+    min-width: 44px;
+    font-size: ${({ theme }) => theme.FONT_SIZE.LG};
+  }
+  .react-calendar__navigation button:enabled:hover,
+  .react-calendar__navigation button:enabled:focus {
+    background-color: ${({ theme }) => theme.PALETTE.GRAY_100};
+  }
+  .react-calendar__navigation button[disabled] {
+    background-color: ${({ theme }) => theme.PALETTE.GRAY_100};
+  }
+  .react-calendar__tile:enabled:hover {
+    background-color: ${({ theme }) => theme.PALETTE.RED_300};
+  }
+  .react-calendar__tile:enabled:focus {
+    background-color: ${({ theme }) => theme.PALETTE.RED_500};
+  }
+  .react-calendar__tile--now {
+    background: none;
+    font-weight: bold;
+  }
+  .react-calendar__tile--now:enabled:hover,
+  .react-calendar__tile--now:enabled:focus {
+    font-weight: bold;
+  }
+  .react-calendar__tile--active {
+    color: white;
+    font-weight: bold;
+    background-color: ${({ theme }) => theme.PALETTE.RED_500};
+  }
+  .react-calendar__tile--active:enabled:hover,
+  .react-calendar__tile--active:enabled:focus {
+    color: white;
+  }
 `;
