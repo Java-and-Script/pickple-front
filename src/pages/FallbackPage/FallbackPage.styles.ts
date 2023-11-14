e75bf98274260c1ea148b6470c1600909e0248d5
dlwl98 <ff981113@hanyang.ac.kr>
2023-11-14 15:10:25 +0900

    feat: FallbackPage 추가

---
+++ src/pages/FallbackPage/FallbackPage.styles.ts
@@ -0,0 +1,6 @@
+import styled from '@emotion/styled';
+
+export const PageWrapper = styled.div`
+  ${({ theme }) => theme.STYLES.LAYOUT}
+  height: 100dvh;
+`;
---
+++ src/pages/FallbackPage/FallbackPage.tsx
@@ -0,0 +1,11 @@
+import { Header } from '@components/Header';
+
+import { PageWrapper } from './FallbackPage.styles';
+
+export const FallbackPage = () => {
+  return (
+    <PageWrapper>
+      <Header isLogo />
+    </PageWrapper>
+  );
+};
