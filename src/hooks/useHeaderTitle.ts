8fb70a3c0b336f59e7f0547af7ec29072238c2b9
dlwl98 <ff981113@hanyang.ac.kr>
2023-11-14 15:09:44 +0900

    fix: useHeaderTitle 훅 수정 - 렌더링중일때 showHeaderTitle값이 true인 버그 수정

--- src/hooks/useHeaderTitle.ts
+++ src/hooks/useHeaderTitle.ts
@@ -2,7 +2,7 @@ import { useIntersectionObserver } from './useIntersectionObserver';
 
 export const useHeaderTitle = <T extends HTMLElement>() => {
   const [entryRef, entry] = useIntersectionObserver<T>({});
-  const showHeaderTitle = !entry?.isIntersecting;
+  const showHeaderTitle = entry && !entry.isIntersecting;
 
   return { entryRef, showHeaderTitle };
 };
