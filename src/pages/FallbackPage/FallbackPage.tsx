a92b1660ad1fee09edfc406871f6c073d9950b14
dlwl98 <ff981113@hanyang.ac.kr>
2023-11-14 15:40:22 +0900

    fix: FallbackPage 헤더 제거

--- src/pages/FallbackPage/FallbackPage.tsx
+++ src/pages/FallbackPage/FallbackPage.tsx
@@ -1,11 +1,5 @@
-import { Header } from '@components/Header';
-
 import { PageWrapper } from './FallbackPage.styles';
 
 export const FallbackPage = () => {
-  return (
-    <PageWrapper>
-      <Header isLogo />
-    </PageWrapper>
-  );
+  return <PageWrapper />;
 };
