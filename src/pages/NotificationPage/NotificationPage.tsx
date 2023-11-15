2783d4babea6faa6c80bb2a4adb0aa3747c3e728
dlwl98 <ff981113@hanyang.ac.kr>
2023-11-15 17:18:54 +0900

    fix: 알림 페이지 스크롤 생기는 버그 수정

--- src/pages/NotificationPage/NotificationPage.tsx
+++ src/pages/NotificationPage/NotificationPage.tsx
@@ -8,7 +8,6 @@ import { useLoginInfoStore } from '@stores/loginInfo.store';
 import {
   ButtonWrapper,
   PageContent,
-  PageLayout,
   PageWrapper,
 } from './NotificationPage.styles';
 
@@ -20,21 +19,19 @@ export const NotificationPage = () => {
   }
 
   return (
-    <PageLayout>
-      <PageWrapper>
-        <Header isLogo={false} title={'알림'} isRightContainer={false} />
-        <ButtonWrapper>
-          <Button
-            {...BUTTON_PROPS.SMALL_GRAY_OUTLINED_BUTTON_PROPS}
-            width="80px"
-            height="32px"
-            onClick={() => {}}
-          >
-            모두 지우기
-          </Button>
-        </ButtonWrapper>
-        <PageContent direction="column" gap={16}></PageContent>
-      </PageWrapper>
-    </PageLayout>
+    <PageWrapper>
+      <Header isLogo={false} title={'알림'} isRightContainer={false} />
+      <ButtonWrapper>
+        <Button
+          {...BUTTON_PROPS.SMALL_GRAY_OUTLINED_BUTTON_PROPS}
+          width="80px"
+          height="32px"
+          onClick={() => {}}
+        >
+          모두 지우기
+        </Button>
+      </ButtonWrapper>
+      <PageContent direction="column" gap={16}></PageContent>
+    </PageWrapper>
   );
 };
