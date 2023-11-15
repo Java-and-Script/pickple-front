925f623cf0048174c20bb516a0335efc68812a62
1g2g <wonji126326@gmail.com>
2023-11-15 21:01:22 +0900

    feat: 메세지 전송 ui 구현

--- src/pages/MessageRoomPage/MessageRoomPage.style.ts
+++ src/pages/MessageRoomPage/MessageRoomPage.style.ts
@@ -11,3 +11,22 @@ export const Main = styled.div`
   gap: 8px;
   margin: 10px 0;
 `;
+
+export const SendButton = styled.button`
+  width: 48px;
+  height: 32px;
+  font-size: ${({ theme }) => theme.FONT_SIZE.XS};
+  background-color: ${({ theme }) => theme.PALETTE.RED_500};
+  color: white;
+  outline: none;
+  border-radius: 8px;
+  border: none;
+`;
+
+export const InputWrapper = styled.div`
+  position: fixed;
+  bottom: 0px;
+  width: 100%;
+  overflow: hidden;
+  background-color: ${({ theme }) => theme.PALETTE.GRAY_200};
+`;
--- src/pages/MessageRoomPage/MessageRoomPage.tsx
+++ src/pages/MessageRoomPage/MessageRoomPage.tsx
@@ -1,26 +1,121 @@
+import { useRef } from 'react';
+
 import { Header } from '@components/Header';
+import { Input } from '@components/shared/Input';
+
+import { theme } from '@styles/theme';
 
 import { Message } from './Message';
-import { Main, MessageRoomContainer } from './MessageRoomPage.style';
+import {
+  InputWrapper,
+  Main,
+  MessageRoomContainer,
+  SendButton,
+} from './MessageRoomPage.style';
 
 export const MessageRoomPage = () => {
+  const ref = useRef<HTMLInputElement | null>(null);
+
+  const sendMessage = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!ref.current) {
+      return;
+    }
+    console.log(ref.current.value);
+
+    ref.current.value = '';
+  };
+
   return (
-    <MessageRoomContainer>
-      <Header
-        isLogo={false}
-        title={'10월 21일 용산구 (8명)'}
-        isRightContainer={true}
-      />
-      <Main>
-        {messages.map((message, index) => (
-          <Message key={index} message={message} isMe={message.id === 1} />
-        ))}
-      </Main>
-    </MessageRoomContainer>
+    <>
+      <MessageRoomContainer>
+        <Header
+          isLogo={false}
+          title={'10월 21일 용산구 (8명)'}
+          isRightContainer={true}
+        />
+        <Main>
+          {messages.map((message, index) => (
+            <Message key={index} message={message} isMe={message.id === 1} />
+          ))}
+        </Main>
+      </MessageRoomContainer>
+      <InputWrapper>
+        <Input
+          onSubmit={sendMessage}
+          height="48px"
+          backgroundColor={theme.PALETTE.GRAY_200}
+          ref={ref}
+        >
+          <SendButton>전송</SendButton>
+        </Input>
+      </InputWrapper>
+    </>
   );
 };
 
 const messages = [
+  {
+    id: 1,
+    name: '원지',
+    content: '안녕안녕안녕안녕안녕 안녕안녕안녕안녕안녕안녕안녕 안녕안녕',
+    time: '20:30',
+  },
+  {
+    id: 1,
+    name: '원지',
+    content: '안녕안녕안녕안녕안녕 안녕안녕안녕안녕안녕안녕안녕 안녕안녕',
+    time: '20:30',
+  },
+  {
+    id: 1,
+    name: '원지',
+    content: '안녕안녕안녕안녕안녕 안녕안녕안녕안녕안녕안녕안녕 안녕안녕',
+    time: '20:30',
+  },
+  {
+    id: 1,
+    name: '원지',
+    content: '안녕안녕안녕안녕안녕 안녕안녕안녕안녕안녕안녕안녕 안녕안녕',
+    time: '20:30',
+  },
+  {
+    id: 1,
+    name: '원지',
+    content: '안녕안녕안녕안녕안녕 안녕안녕안녕안녕안녕안녕안녕 안녕안녕',
+    time: '20:30',
+  },
+  {
+    id: 1,
+    name: '원지',
+    content: '안녕안녕안녕안녕안녕 안녕안녕안녕안녕안녕안녕안녕 안녕안녕',
+    time: '20:30',
+  },
+  {
+    id: 1,
+    name: '원지',
+    content: '안녕안녕안녕안녕안녕 안녕안녕안녕안녕안녕안녕안녕 안녕안녕',
+    time: '20:30',
+  },
+  {
+    id: 1,
+    name: '원지',
+    content: '안녕안녕안녕안녕안녕 안녕안녕안녕안녕안녕안녕안녕 안녕안녕',
+    time: '20:30',
+  },
+  {
+    id: 1,
+    name: '원지',
+    content: '안녕안녕안녕안녕안녕 안녕안녕안녕안녕안녕안녕안녕 안녕안녕',
+    time: '20:30',
+  },
+  {
+    id: 1,
+    name: '원지',
+    content: '안녕안녕안녕안녕안녕 안녕안녕안녕안녕안녕안녕안녕 안녕안녕',
+    time: '20:30',
+  },
   {
     id: 1,
     name: '원지',
