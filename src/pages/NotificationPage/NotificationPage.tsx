a1fc2f2a700c9f874f5d728219c20c15336145c0
김민재 <kimminje7267@naver.com>
2023-11-15 12:50:09 +0900

    feat: 알림 페이지 NotificationItem 컴포넌트 사용

--- src/pages/NotificationPage/NotificationPage.tsx
+++ src/pages/NotificationPage/NotificationPage.tsx
@@ -1,5 +1,7 @@
 import { Header } from '@components/Header';
+import { NotificationItem } from '@components/NotificationItem';
 import { Button } from '@components/shared/Button';
+import { Text } from '@components/shared/Text';
 
 import { BUTTON_PROPS } from '@styles/button';
 
@@ -22,20 +24,52 @@ export const NotificationPage = () => {
   return (
     <PageLayout>
       <PageWrapper>
-        <Header isLogo={false} title={'알림'} />
+        <Header isLogo={false} title={'알림'} isRightContainer={false} />
+        <ButtonWrapper>
+          <Button
+            {...BUTTON_PROPS.SMALL_GRAY_OUTLINED_BUTTON_PROPS}
+            width="80px"
+            height="32px"
+            onClick={() => {}}
+          >
+            모두 지우기
+          </Button>
+        </ButtonWrapper>
         <PageContent direction="column" gap={16}>
-          <ButtonWrapper>
-            <Button
-              {...BUTTON_PROPS.SMALL_GRAY_OUTLINED_BUTTON_PROPS}
-              width="80px"
-              height="32px"
-              onClick={() => {
-                console.log('알림 모두 지우기');
-              }}
-            >
-              모두 지우기
-            </Button>
-          </ButtonWrapper>
+          <NotificationItem
+            box={<NotificationItem.Image src="asd" />}
+            title="뜨거운 백둥사형제"
+            createdAt={new Date()}
+            content={
+              <span>
+                <Text size={14}>크루가입</Text>
+                <Text size={14} weight={300}>
+                  이 수락되었어요.
+                </Text>
+              </span>
+            }
+            read
+            onClick={() => {}}
+          />
+          <NotificationItem
+            box={
+              <NotificationItem.MatchStatus
+                startTime={new Date()}
+                timeMinutes={90}
+              />
+            }
+            title="10.21 송파구"
+            createdAt={new Date()}
+            content={
+              <span>
+                <Text size={14}>게스트 모집 참여 수락</Text>
+                <Text size={14} weight={300}>
+                  을 기다리고 있어요.
+                </Text>
+              </span>
+            }
+            onClick={() => {}}
+          />
         </PageContent>
       </PageWrapper>
     </PageLayout>
