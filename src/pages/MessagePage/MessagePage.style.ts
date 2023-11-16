3b264a216d6b7badac441f700a88250f5c89f10d
1g2g <wonji126326@gmail.com>
2023-11-16 12:40:12 +0900

    feat: 메세지 목록 ui 구현

--- src/pages/MessagePage/MessagePage.style.ts
+++ src/pages/MessagePage/MessagePage.style.ts
@@ -1,6 +1,8 @@
 import styled from '@emotion/styled';
 
 import { Flex } from '@components/shared/Flex';
+import { Image } from '@components/shared/Image';
+import { Text } from '@components/shared/Text';
 
 export const MessageContainer = styled.div`
   ${({ theme }) => theme.STYLES.LAYOUT}
@@ -23,4 +25,17 @@ export const TabBarButton = styled.button<{ isSelected: boolean }>`
   border-bottom: ${({ isSelected }) =>
     isSelected ? ' 1px solid black' : ' 1px solid white'};
   padding: 10px;
+`;
+
+export const MessageRoomItem = styled(Flex)``;
+
+export const MessageItemAvatar = styled(Image)`
+  border-radius: 4px;
+  border: 1px solid ${({ theme }) => theme.PALETTE.GRAY_400};
+`;
+export const Nickname = styled(Text)`
+  width: 100%;
+`;
+export const DateText = styled(Text)`
+  min-width: 70px;
 `;
--- src/pages/MessagePage/MessagePage.tsx
+++ src/pages/MessagePage/MessagePage.tsx
@@ -1,18 +1,56 @@
 import { useState } from 'react';
 
 import { Header } from '@components/Header';
+import { Flex } from '@components/shared/Flex';
+import { Text } from '@components/shared/Text';
+
+import { theme } from '@styles/theme';
 
 import {
+  DateText,
   Main,
   MessageContainer,
+  MessageItemAvatar,
+  MessageRoomItem,
+  Nickname,
   TabBar,
   TabBarButton,
 } from './MessagePage.style.ts';
 
-const TAB_TITLE = { ONE_TO_ONE: 'oneToOne', GROUP: 'group' };
+const TEMP_TAB_TITLE = { ONE_TO_ONE: 'one', GROUP: 'crew, game' };
+
+type MessageRoom = {
+  nickname: string;
+  content: string;
+  date: string;
+  img: string;
+  type: string;
+};
+
+const MessageRoom = ({ messageRoom }: { messageRoom: MessageRoom }) => {
+  const { nickname, content, date, img } = messageRoom;
+  return (
+    <MessageRoomItem justify="space-between">
+      <Flex gap={8}>
+        <MessageItemAvatar width="40" alt="avatar" src={img} />
+        <Flex direction="column">
+          <Nickname size={12} weight={500} ellipsis={1}>
+            {nickname}
+          </Nickname>
+          <Text size={12} weight={300} ellipsis={1}>
+            {content}
+          </Text>
+        </Flex>
+      </Flex>
+      <DateText size={8} color={theme.PALETTE.GRAY_500}>
+        {date}
+      </DateText>
+    </MessageRoomItem>
+  );
+};
 
 export const MessagePage = () => {
-  const [selectedTab, setSelectedTab] = useState(TAB_TITLE.ONE_TO_ONE);
+  const [selectedTab, setSelectedTab] = useState(TEMP_TAB_TITLE.ONE_TO_ONE);
 
   const handleTabClick = (tab: string) => {
     setSelectedTab(tab);
@@ -23,20 +61,46 @@ export const MessagePage = () => {
       <Header isLogo={false} title="메세지" isRightContainer={true} />
       <Main>
         <TabBar>
-          <TabBarButton
-            onClick={() => handleTabClick(TAB_TITLE.ONE_TO_ONE)}
-            isSelected={selectedTab === TAB_TITLE.ONE_TO_ONE}
-          >
-            일대일
-          </TabBarButton>
-          <TabBarButton
-            onClick={() => handleTabClick(TAB_TITLE.GROUP)}
-            isSelected={selectedTab === TAB_TITLE.GROUP}
-          >
-            그룹
-          </TabBarButton>
+          {Object.values(TEMP_TAB_TITLE).map((tabTitle) => (
+            <TabBarButton
+              key={tabTitle}
+              onClick={() => handleTabClick(tabTitle)}
+              isSelected={selectedTab === tabTitle}
+            >
+              {tabTitle === TEMP_TAB_TITLE.ONE_TO_ONE ? '일대일' : '그룹'}
+            </TabBarButton>
+          ))}
         </TabBar>
+        {data
+          .filter(({ type }) => selectedTab.includes(type))
+          .map((messageRoom) => (
+            <MessageRoom messageRoom={messageRoom} />
+          ))}
       </Main>
     </MessageContainer>
   );
 };
+
+const data: MessageRoom[] = [
+  {
+    nickname: '강원지',
+    content: '아아아아아아',
+    date: '2023년 11월 16일',
+    img: 'https://velog.velcdn.com/images/sharphand1/post/e7f981a1-fe04-4687-800a-f7e411e6abff/image.png',
+    type: 'game',
+  },
+  {
+    nickname: '강원지',
+    content: '아아아아아아',
+    date: '2023년 11월 16일',
+    img: 'https://velog.velcdn.com/images/sharphand1/post/e7f981a1-fe04-4687-800a-f7e411e6abff/image.png',
+    type: 'crew',
+  },
+  {
+    nickname: '강원지',
+    content: '아아아아아아',
+    date: '2023년 11월 16일',
+    img: 'https://velog.velcdn.com/images/sharphand1/post/e7f981a1-fe04-4687-800a-f7e411e6abff/image.png',
+    type: 'one',
+  },
+];
