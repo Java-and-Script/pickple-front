c55d5131d4da63653121a9a2c9ba5b73d396071b
Minjae Kim <kimminje7267@naver.com>
2023-11-16 13:48:04 +0900

    EventSource 에 연결하는 훅 구현 (#214)
    * chore: eventSource 관련 의존성 설치
    
    Co-authored-by: 이진욱 <ff981113@hanyang.ac.kr>
    
    * chore: BaseURL 분리
    
    Co-authored-by: 이진욱 <ff981113@hanyang.ac.kr>
    
    * feat: useEventSource Hook 구현
    
    ---------
    
    Co-authored-by: 이진욱 <ff981113@hanyang.ac.kr>

--- src/consts/network.ts
+++ src/consts/network.ts
@@ -1,1 +1,4 @@
+const PROD = import.meta.env.MODE === 'production';
+export const BASE_URL = PROD ? import.meta.env.VITE_BASE_URL : '/api';
+
 export const FETCH_SIZE = 20;
---
+++ src/hooks/useEventSource.ts
@@ -0,0 +1,32 @@
+import { useEffect } from 'react';
+
+import { EventSourcePolyfill, NativeEventSource } from 'event-source-polyfill';
+
+import { useLoginInfoStore } from '@stores/loginInfo.store';
+
+export const useEventSource = (
+  subscribeUrl: string,
+  onmessage: EventSourcePolyfill['onmessage'],
+  onerror: EventSourcePolyfill['onerror']
+) => {
+  const loginInfo = useLoginInfoStore((state) => state.loginInfo);
+
+  useEffect(() => {
+    if (!loginInfo?.id) {
+      return;
+    }
+
+    const EventSource = EventSourcePolyfill || NativeEventSource;
+    const eventSource = new EventSource(subscribeUrl, {
+      headers: { Authorization: loginInfo.accessToken },
+    });
+
+    eventSource.onmessage = onmessage;
+
+    eventSource.onerror = onerror;
+
+    return () => {
+      eventSource.close();
+    };
+  }, [loginInfo, onmessage, onerror, subscribeUrl]);
+};
