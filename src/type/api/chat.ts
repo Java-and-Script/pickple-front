f921ff3d75b6218b7511f62254ff2340869515de
1g2g <wonji126326@gmail.com>
2023-11-20 17:45:53 +0900

    fix: 채팅 api 타입 변경

---
+++ src/type/api/chat.ts
@@ -0,0 +1,37 @@
+import { ChatMessage } from '@type/models/ChatMessage';
+import { ChatRoom, ChatRoomDetail } from '@type/models/ChatRoom';
+
+export type PostCreatePersonalChatRoomRequest = {
+  receiverId: number;
+};
+
+export type PostCreatePersonalChatRoomResponse = ChatRoomDetail;
+
+export type GetPersonalChatRoomExistedRequest = {
+  receiverId: number;
+};
+
+export type GetPersonalChatRoomExistedResponse = { existed: boolean };
+
+export type GetAllChatRoomListRequest = {
+  type: ChatRoom['type'];
+};
+
+export type GetAllChatRoomListResponse = ChatRoom[];
+
+export type GetChatRoomDetailsRequest = {
+  roomId: number;
+};
+
+export type GetChatRoomDetailsResponse = ChatRoomDetail;
+
+export type GetAllChatMessagesRequest = {
+  roomId: number;
+};
+
+export type GetAllChatMessagesResponse = ChatMessage[];
+
+export type SendMessageRequest = {
+  senderId: string | number;
+  content: string | null;
+};
