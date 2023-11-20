e0d9cdfaae6913f18f10516ce8457b798c6c5162
1g2g <wonji126326@gmail.com>
2023-11-20 23:33:39 +0900

    feat: 채팅 목록 탭 name 상수화

---
+++ src/consts/chatRoomTabTitle.ts
@@ -0,0 +1,7 @@
+import { ChatRoom } from '@type/models/ChatRoom';
+
+export const CHAT_ROOM_TAB_TITLE = {
+  INDIVIDUAL: '개인' as ChatRoom['type'],
+  GUEST: '게스트' as ChatRoom['type'],
+  CREW: '크루' as ChatRoom['type'],
+};
