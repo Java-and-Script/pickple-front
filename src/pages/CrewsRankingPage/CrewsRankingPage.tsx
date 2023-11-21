c56098a6d5781380f2d149e4e4cc2b4a295f78c6
dlwl98 <ff981113@hanyang.ac.kr>
2023-11-21 14:05:41 +0900

    feat: 크루 랭킹 페이지에 api 연경

--- src/pages/CrewsRankingPage/CrewsRankingPage.tsx
+++ src/pages/CrewsRankingPage/CrewsRankingPage.tsx
@@ -1,4 +1,5 @@
-import { useRef, useState } from 'react';
+import { useState } from 'react';
+import { flushSync } from 'react-dom';
 import { useNavigate } from 'react-router-dom';
 
 import { Header } from '@components/Header';
@@ -6,6 +7,10 @@ import { Modal } from '@components/Modal';
 import { RankingModalContent } from '@components/RankingModalContent';
 import { Flex } from '@components/shared/Flex';
 import { Text } from '@components/shared/Text';
+
+import { useCrewsRankingQuery } from '@hooks/queries/useCrewsRankingQuery';
+
+import { CrewRank } from '@type/models/CrewRank';
 
 import { PATH_NAME } from '@consts/pathName';
 
@@ -19,16 +24,21 @@ import { RankingItem } from './components/RankingItem';
 export const CrewsRankingPage = () => {
   const navigate = useNavigate();
   const [isOpen, setIsOpen] = useState(false);
-  const crewIdRef = useRef<number | null>(null);
+  const [selectedCrewRank, setSelectedCrewRank] = useState<CrewRank | null>(
+    null
+  );
 
-  const openModal = (crewId: number) => {
+  const { data: crewsRanking } = useCrewsRankingQuery();
+
+  const openModal = (crewRank: CrewRank) => {
     setIsOpen(true);
-    crewIdRef.current = crewId;
+    setSelectedCrewRank(crewRank);
   };
 
   const handleDetailButtonClick = () => {
-    if (crewIdRef.current) {
-      navigate(PATH_NAME.GET_CREWS_PATH(String(crewIdRef.current)));
+    if (selectedCrewRank) {
+      flushSync(() => setIsOpen(false));
+      navigate(PATH_NAME.GET_CREWS_PATH(String(selectedCrewRank.id)));
     }
   };
 
@@ -49,52 +59,36 @@ export const CrewsRankingPage = () => {
             점수
           </Text>
         </RankingHeader>
-        <RankingItem
-          rank={1}
-          name="민재 크루"
-          profilImageUrl="https://picsum.photos/41"
-          rating={20000}
-          onClick={() => openModal(1)}
-        />
-        <RankingItem
-          rank={2}
-          name="찬 크루"
-          profilImageUrl="https://picsum.photos/42"
-          rating={10000}
-          onClick={() => openModal(2)}
-        />
-        <RankingItem
-          rank={3}
-          name="원지 크루"
-          profilImageUrl="https://picsum.photos/43"
-          rating={5000}
-          onClick={() => openModal(3)}
-        />
-        <RankingItem
-          rank={49}
-          name="진욱 크루"
-          profilImageUrl="https://picsum.photos/44"
-          rating={1000}
-          onClick={() => openModal(49)}
-        />
+        {crewsRanking.map((crewRank) => (
+          <RankingItem
+            key={crewRank.id}
+            rank={crewRank.rank}
+            name={crewRank.name}
+            profilImageUrl={crewRank.profileImageUrl}
+            rating={crewRank.totalScore}
+            onClick={() => openModal(crewRank)}
+          />
+        ))}
       </PageContent>
 
       <Modal header={20} isOpen={isOpen} close={() => setIsOpen(false)}>
         <Modal.Content>
-          <RankingModalContent
-            profileImageUrl="asd"
-            name="민재크루"
-            ranking={49}
-            infoText="랭킹은 활동점수와 매너지수를 기반으로 선정됩니다"
-            activityScore={10000}
-            mannerScore={10000}
-            totalScore={20000}
-            addressDepth1="서울시"
-            addressDepth2="강남구"
-            memberCount={3}
-            maxMemberCount={10}
-            onDetailButtonClick={handleDetailButtonClick}
-          />
+          {selectedCrewRank && (
+            <RankingModalContent
+              profileImageUrl={selectedCrewRank.profileImageUrl}
+              name={selectedCrewRank.name}
+              ranking={selectedCrewRank.rank}
+              infoText="랭킹은 활동점수와 매너지수를 기반으로 선정됩니다"
+              activityScore={selectedCrewRank.crewActivityScore}
+              mannerScore={selectedCrewRank.mannerScore}
+              totalScore={selectedCrewRank.totalScore}
+              addressDepth1={selectedCrewRank.addressDepth1}
+              addressDepth2={selectedCrewRank.addressDepth2}
+              memberCount={selectedCrewRank.memberCount}
+              maxMemberCount={selectedCrewRank.maxMemberCount}
+              onDetailButtonClick={handleDetailButtonClick}
+            />
+          )}
         </Modal.Content>
       </Modal>
     </PageWrapper>
