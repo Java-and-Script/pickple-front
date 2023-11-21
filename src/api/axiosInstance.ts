0f19d1983eda63f10163d49c8fdce1e23461f6ec
dlwl98 <ff981113@hanyang.ac.kr>
2023-11-21 18:12:45 +0900

    feat: accessToken refresh 하는 axios interceptor 구현

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -2,13 +2,15 @@ import axios from 'axios';
 
 import { BASE_URL } from '@consts/network';
 
-import { setAuthorization } from './interceptors';
+import { handleAuthError, setAuthorization } from './interceptors';
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
   headers: {
     'Content-type': 'application/json',
   },
+  withCredentials: true,
 });
 
 axiosInstance.interceptors.request.use(setAuthorization);
+axiosInstance.interceptors.response.use((res) => res, handleAuthError);
