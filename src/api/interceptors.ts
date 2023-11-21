c3744baa3762c7521de93005ca3b1bb311bb1ac2
이진욱 <ff981113@hanyang.ac.kr>
2023-11-21 18:16:34 +0900

    feat: accessToken refresh 하는 axios interceptor 구현 (#276)

--- src/api/interceptors.ts
+++ src/api/interceptors.ts
@@ -1,6 +1,11 @@
-import { InternalAxiosRequestConfig } from 'axios';
+import { AxiosError, InternalAxiosRequestConfig } from 'axios';
 
 import { useTokenStore } from '@stores/accessToken.store';
+
+import { CommonErrorResponse } from '@type/api/error';
+
+import { axiosInstance } from './axiosInstance';
+import { postRefreshAccessToken } from './member/postRefreshAccessToken';
 
 export const setAuthorization = (config: InternalAxiosRequestConfig) => {
   const accessToken = useTokenStore.getState().accessToken;
@@ -14,4 +19,19 @@ export const setAuthorization = (config: InternalAxiosRequestConfig) => {
   }
 
   return config;
+};
+
+export const handleAuthError = async (
+  error: AxiosError<CommonErrorResponse>
+) => {
+  const data = error.response?.data;
+
+  if (data?.code === 'AUT-002') {
+    const { accessToken } = await postRefreshAccessToken();
+    useTokenStore.getState().setAccessToken(accessToken);
+  }
+
+  if (error.config) {
+    return axiosInstance(error.config);
+  }
 };
