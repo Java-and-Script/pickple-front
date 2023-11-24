4d2f71d23fb7c75d2fd3436a270d6a91a519b69e
1g2g <87280835+1g2g@users.noreply.github.com>
2023-11-24 14:22:10 +0900

    채팅 목록 페이지 쿼리 수정 (#317)
    * fix: 클릭된 탭의 채팅 목록만 fetch하도록 변경
    
    * fix: 채팅 목록 쿼리 refetchInterval 설정
    
    * chore: 변경된 변수명 적용 및 map 함수 key 변경
    
    * feat: 채팅방 목록 시간 순 정렬 구현

--- src/pages/ChatRoomListPage/useChatRoomListPage.ts
+++ src/pages/ChatRoomListPage/useChatRoomListPage.ts
@@ -1,4 +1,3 @@
-import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { useAllChatRoomListQuery } from '@hooks/queries/useAllChatRoomListQuery.ts';
@@ -8,8 +7,6 @@ import { useLoginInfoStore } from '@stores/loginInfo.store';
 
 import { ChatRoom } from '@type/models/ChatRoom.ts';
 
-import { CHAT_ROOM_TAB_TITLE } from '@consts/chatRoomTabTitle.ts';
-
 export const useChatRoomListPage = () => {
   const navigate = useNavigate();
 
@@ -18,19 +15,12 @@ export const useChatRoomListPage = () => {
     throw new Error('로그인이 필요한 서비스입니다.');
   }
 
-  const { data: individualRooms } = useAllChatRoomListQuery({
-    type: CHAT_ROOM_TAB_TITLE.INDIVIDUAL,
-  });
-  const { data: guestRooms } = useAllChatRoomListQuery({
-    type: CHAT_ROOM_TAB_TITLE.GUEST,
-  });
-  const { data: crewRooms } = useAllChatRoomListQuery({
-    type: CHAT_ROOM_TAB_TITLE.CREW,
-  });
-
   const { chatRoomTab, setChatRoomTab } = useChatRoomTabStore();
 
-  const [selectedData, setSelectedData] = useState(individualRooms);
+  const { data: selectedTabChatRoomList, refetch: refetchChatList } =
+    useAllChatRoomListQuery({
+      type: chatRoomTab,
+    });
 
   const moveToPage = (pathName: string) => {
     navigate(pathName);
@@ -38,17 +28,24 @@ export const useChatRoomListPage = () => {
 
   const handleClickTab = (tab: ChatRoom['type']) => {
     setChatRoomTab(tab);
+
+    refetchChatList();
   };
 
-  useEffect(() => {
-    if (chatRoomTab === CHAT_ROOM_TAB_TITLE.INDIVIDUAL) {
-      setSelectedData(individualRooms);
-    } else if (chatRoomTab === CHAT_ROOM_TAB_TITLE.GUEST) {
-      setSelectedData(guestRooms);
-    } else {
-      setSelectedData(crewRooms);
-    }
-  }, [individualRooms, guestRooms, crewRooms, chatRoomTab]);
+  return {
+    selectedTabChatRoomList: sortDate(selectedTabChatRoomList),
+    chatRoomTab,
+    moveToPage,
+    handleClickTab,
+  };
+};
 
-  return { selectedData, chatRoomTab, moveToPage, handleClickTab };
+const sortDate = (chatRooms: ChatRoom[]) => {
+  chatRooms.sort(
+    (prev: ChatRoom, cur: ChatRoom) =>
+      new Date(cur.lastMessageCreatedAt).getTime() -
+      new Date(prev.lastMessageCreatedAt).getTime()
+  );
+
+  return chatRooms;
 };
