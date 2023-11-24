f9893ea291009ea9032f3e20703d233cc99e54a7
dlwl98 <ff981113@hanyang.ac.kr>
2023-11-24 16:47:32 +0900

    fix: api 명세 확정 안된 부분 주석처리

--- src/hooks/mutations/useGameAlarmsPatchMutation.ts
+++ src/hooks/mutations/useGameAlarmsPatchMutation.ts
@@ -1,47 +1,41 @@
-import {
-  InfiniteData,
-  useMutation,
-  useQueryClient,
-} from '@tanstack/react-query';
+import { useMutation } from '@tanstack/react-query';
 
 import { patchGameAlarms } from '@api/alarms/patchGameAlarms';
 
-import { GetAlarmsResponse } from '@type/api/alarm';
-
 export const useGameAlarmsPatchMutation = () => {
-  const queryClient = useQueryClient();
+  // const queryClient = useQueryClient();
 
   return useMutation({
     mutationFn: (alarmId: number) => patchGameAlarms(alarmId),
-    onSuccess: (_, alarmId) => {
-      const previousAlarms = queryClient.getQueryData<
-        InfiniteData<GetAlarmsResponse, unknown>
-      >(['alarms']);
+    // onSuccess: (_, alarmId) => {
+    //   const previousAlarms = queryClient.getQueryData<
+    //     InfiniteData<GetAlarmsResponse, unknown>
+    //   >(['alarms']);
 
-      if (previousAlarms) {
-        const { pages, pageParams } = previousAlarms;
-        const newPages: GetAlarmsResponse[] = pages.map((page) => {
-          const alarm = page.alarms.find((alarm) => alarm.id === alarmId);
-          if (!alarm) {
-            return page;
-          } else {
-            return {
-              ...page,
-              alarms: page.alarms.map((alarm) => {
-                if (alarm.id === alarmId) {
-                  return { ...alarm, status: 'read' };
-                } else {
-                  return alarm;
-                }
-              }),
-            };
-          }
-        });
-        queryClient.setQueryData<unknown, string[], typeof previousAlarms>(
-          ['alarms'],
-          { pageParams, pages: newPages }
-        );
-      }
-    },
+    //   if (previousAlarms) {
+    //     const { pages, pageParams } = previousAlarms;
+    //     const newPages: GetAlarmsResponse[] = pages.map((page) => {
+    //       const alarm = page.alarms.find((alarm) => alarm.id === alarmId);
+    //       if (!alarm) {
+    //         return page;
+    //       } else {
+    //         return {
+    //           ...page,
+    //           alarms: page.alarms.map((alarm) => {
+    //             if (alarm.id === alarmId) {
+    //               return { ...alarm, status: 'read' };
+    //             } else {
+    //               return alarm;
+    //             }
+    //           }),
+    //         };
+    //       }
+    //     });
+    //     queryClient.setQueryData<unknown, string[], typeof previousAlarms>(
+    //       ['alarms'],
+    //       { pageParams, pages: newPages }
+    //     );
+    //   }
+    // },
   });
 };
