8818dffeb84593fc4e77eb9d0f06a71ba1fdc621
1g2g <wonji126326@gmail.com>
2023-11-24 15:40:27 +0900

    fix: 채팅 목록 탭 전환 시 깜빡거리는 버그 해결

--- src/pages/ChatRoomListPage/ChatRoomListPage.tsx
+++ src/pages/ChatRoomListPage/ChatRoomListPage.tsx
@@ -1,26 +1,21 @@
+import { Suspense } from 'react';
+
 import { Header } from '@components/Header';
-import { Flex } from '@components/shared/Flex';
 
-import { theme } from '@styles/theme.ts';
-
-import { ChatRoom } from '@type/models/ChatRoom.ts';
+import { useChatRoomTabStore } from '@stores/chatRoomTab.store.ts';
 
 import { CHAT_ROOM_TAB_TITLE } from '@consts/chatRoomTabTitle.ts';
-import { PATH_NAME } from '@consts/pathName.ts';
 
-import { ChatRoomItem } from './ChatRoomItem.tsx';
+import { ChatRoomList } from './ChatRoomList.tsx';
 import {
-  InformText,
   Main,
   MessagePageContainer,
   TabBar,
   TabBarButton,
 } from './ChatRoomListPage.style.ts';
-import { useChatRoomListPage } from './useChatRoomListPage.ts';
 
 export const ChatRoomListPage = () => {
-  const { selectedTabChatRoomList, chatRoomTab, moveToPage, handleClickTab } =
-    useChatRoomListPage();
+  const { chatRoomTab, setChatRoomTab } = useChatRoomTabStore();
 
   return (
     <MessagePageContainer>
@@ -29,7 +24,7 @@ export const ChatRoomListPage = () => {
         <TabBar>
           {Object.values(CHAT_ROOM_TAB_TITLE).map((tab) => (
             <TabBarButton
-              onClick={() => handleClickTab(tab)}
+              onClick={() => setChatRoomTab(tab)}
               isSelected={chatRoomTab === tab}
               key={tab}
             >
@@ -37,23 +32,9 @@ export const ChatRoomListPage = () => {
             </TabBarButton>
           ))}
         </TabBar>
-        {selectedTabChatRoomList.length !== 0 ? (
-          selectedTabChatRoomList.map((chatRoomItem: ChatRoom) => (
-            <ChatRoomItem
-              chatRoomItem={chatRoomItem}
-              key={chatRoomItem.id}
-              onClickChatRoomItem={() =>
-                moveToPage(PATH_NAME.GET_CHAT_PATH(String(chatRoomItem.id)))
-              }
-            />
-          ))
-        ) : (
-          <Flex justify="center" gap={16}>
-            <InformText size={theme.FONT_SIZE.XS} weight={300}>
-              채팅 내역이 없습니다.
-            </InformText>
-          </Flex>
-        )}
+        <Suspense fallback={null}>
+          <ChatRoomList />
+        </Suspense>
       </Main>
     </MessagePageContainer>
   );
