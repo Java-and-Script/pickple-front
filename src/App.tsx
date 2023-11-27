706ad739cd499f4d31a8003bcaf4f52cb0afd0ec
이진욱 <ff981113@hanyang.ac.kr>
2023-11-27 18:42:32 +0900

    fix: EventSource subscribe url 수정 (#354)

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ const queryClient = new QueryClient({
 
 function App() {
   useEventSource(
-    '/api/alarms/subscribe',
+    `${import.meta.env.VITE_BASE_URL}/alarms/subscribe`,
     () => {
       queryClient.resetQueries({ queryKey: ['alarms'] });
       queryClient
