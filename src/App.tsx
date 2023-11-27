41a8fff2377d8578ebb6a72c16cc996f7f188639
dlwl98 <ff981113@hanyang.ac.kr>
2023-11-27 18:22:02 +0900

    feat: sse 메세지 왔을 때 toast해주는 기능

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import { Toaster } from 'react-hot-toast';
+import toast, { Toaster } from 'react-hot-toast';
 import { RouterProvider } from 'react-router-dom';
 
 import { ThemeProvider } from '@emotion/react';
-import { QueryClient } from '@tanstack/react-query';
+import { InfiniteData, QueryClient } from '@tanstack/react-query';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
@@ -12,6 +12,8 @@ import { useEventSource } from '@hooks/useEventSource';
 
 import GlobalStyle from '@styles/globalStyle';
 import { theme } from '@styles/theme';
+
+import { GetAlarmsResponse } from '@type/api/alarm';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -27,7 +29,20 @@ function App() {
     '/api/alarms/subscribe',
     () => {
       queryClient.resetQueries({ queryKey: ['alarms'] });
-      queryClient.invalidateQueries({ queryKey: ['alarms-unread'] });
+      queryClient
+        .invalidateQueries({ queryKey: ['alarms-unread'] })
+        .then(() => {
+          const data = queryClient.getQueryData<
+            InfiniteData<GetAlarmsResponse, number>
+          >(['alarms']);
+          const alarms = data?.pages.flatMap((page) => page.alarmResponse);
+          alarms &&
+            toast(
+              'crewId' in alarms[0]
+                ? alarms[0].crewAlarmMessage
+                : alarms[0].gameAlarmMessage
+            );
+        });
     },
     (error) => console.log(error)
   );
