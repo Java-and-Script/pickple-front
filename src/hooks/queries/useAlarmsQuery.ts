bcdf0ea3d4329ac0c810cfd9b43ab552fb3eb0d1
김민재 <kimminje7267@naver.com>
2023-11-27 17:16:56 +0900

    fix: alarm 조회 query hook 수정
    Co-authored-by: 이진욱 <ff981113@hanyang.ac.kr>

--- src/hooks/queries/useAlarmsQuery.ts
+++ src/hooks/queries/useAlarmsQuery.ts
@@ -8,17 +8,16 @@ export const useAlarmsQuery = () => {
   return useSuspenseInfiniteQuery({
     queryKey: ['alarms'],
     queryFn: ({ pageParam }) =>
-      getAlarms({ cursorId: pageParam, size: FETCH_SIZE }),
+      getAlarms({
+        cursorId: pageParam !== 0 ? pageParam : undefined,
+        size: FETCH_SIZE,
+      }),
 
     getNextPageParam: (lastPage) => {
       if (!lastPage.hasNext) {
         return undefined;
       }
-      const lastAlarm = lastPage.alarms[lastPage.alarms.length - 1];
-      if ('crewId' in lastAlarm) {
-        return lastAlarm.crewId;
-      }
-      return lastAlarm.gameId;
+      return lastPage.cursorId;
     },
     initialPageParam: 0,
   });
