433b8216f8032d1973d200ff8ac1742917416ebc
1g2g <wonji126326@gmail.com>
2023-11-28 01:00:12 +0900

    feat: 지도로 보기 구현

--- src/pages/GamesDetailPage/GamesDetailPage.tsx
+++ src/pages/GamesDetailPage/GamesDetailPage.tsx
@@ -31,6 +31,7 @@ import Ball from '@assets/ball.svg';
 import GameMember from '@assets/gameMember.svg';
 import Money from '@assets/money.svg';
 
+import { GameLocation } from './GameLocation';
 import {
   ButtonWrapper,
   GrayText,
@@ -255,6 +256,10 @@ export const GamesDetailPage = () => {
             ))}
           </Guests>
         </GuestsContainer>
+        <Text size={20} weight={700}>
+          지도로 보기
+        </Text>
+        <GameLocation match={match} />
         <ButtonWrapper>
           {loginInfo && !isStarted && canParticipate && (
             <ErrorBoundary
