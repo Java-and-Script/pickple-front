b18be161d9a7406d04107af42efe1f0f39845899
1g2g <87280835+1g2g@users.noreply.github.com>
2023-11-28 18:33:18 +0900

    게스트/크루 참여 수락/거절 시 토스트 구현 (#384)
    * feat: 게임 참여/거절 요청에 대한 토스트 구현
    
    * feat: 크루 가입 수락/거절 요청에 대한 토스트 구현

--- src/hooks/mutations/useDisallowCrewParticipateMutation.ts
+++ src/hooks/mutations/useDisallowCrewParticipateMutation.ts
@@ -1,3 +1,5 @@
+import toast from 'react-hot-toast';
+
 import { useMutation } from '@tanstack/react-query';
 import { useQueryClient } from '@tanstack/react-query';
 
@@ -9,11 +11,16 @@ export const useDisallowCrewParticipateMutation = () => {
   return useMutation({
     mutationFn: deleteCrewParticipate,
     onSuccess: (_, variables) => {
+      toast.success('크루 가입을 거절했습니다.');
+
       const { crewId } = variables;
 
       queryClient.invalidateQueries({
         queryKey: ['crew-members', crewId, '대기'],
       });
+    },
+    onError: () => {
+      toast.error('크루 가입를 거절하지 못 했습니다. 다시 한 번 시도해주세요.');
     },
   });
 };
