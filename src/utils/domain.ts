8e27ac9b66d11d3445b480fa783f4ede399d8080
dlwl98 <ff981113@hanyang.ac.kr>
2023-12-01 16:00:18 +0900

    feat: 리뷰남기기 가능 기간인지 확인하는 도메인 유틸함수 작성

--- src/utils/domain.ts
+++ src/utils/domain.ts
@@ -20,3 +20,16 @@ export const isGameEnded = (startDate: Date, playTimeMinutes: number) => {
   const endTimeNumber = startDate.getTime() + playTimeMinutes * 60000;
   return endTimeNumber <= new Date().getTime();
 };
+
+export const isReviewPeriod = (
+  gamePlayDate: string,
+  gamePlayStartTime: string,
+  playTimeMinutes: number
+) => {
+  const gameStartDate = getGameStartDate(gamePlayDate, gamePlayStartTime);
+  const gameEndTimeNumber = gameStartDate.getTime() + playTimeMinutes * 60000;
+  const oneWeekMs = 1000 * 60 * 60 * 24 * 7;
+  const now = new Date();
+
+  return now.getTime() - gameEndTimeNumber < oneWeekMs;
+};
