87fe6b6b99714405ba45da95ace31c2c7dbe98bb
dlwl98 <ff981113@hanyang.ac.kr>
2023-12-01 15:59:48 +0900

    feat: 매너스코어 뮤테이션에 관련 쿼리 무효화 로직 추가

--- src/hooks/mutations/useMannerScoreReviewPatchMutation.ts
+++ src/hooks/mutations/useMannerScoreReviewPatchMutation.ts
@@ -1,6 +1,8 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 import { patchMannerScoreReview } from '@api/games/patchMannerScoreReview';
+
+import { useLoginInfoStore } from '@stores/loginInfo.store';
 
 import { PatchGameMannerScoreReviewRequest } from '@type/api/games';
 import { Game } from '@type/models';
@@ -12,8 +14,23 @@ export const useMannerScoreReviewPatchMutation = ({
   payload: PatchGameMannerScoreReviewRequest;
   gameId: Game['id'];
 }) => {
+  const queryClient = useQueryClient();
+  const loginInfo = useLoginInfoStore((state) => state.loginInfo);
+
   return useMutation({
     mutationKey: ['patch-manner-score-review', gameId, JSON.stringify(payload)],
     mutationFn: () => patchMannerScoreReview({ payload, gameId }),
+    onSuccess: () => {
+      console.log(['game-registration', loginInfo?.id, gameId]);
+      queryClient.invalidateQueries({
+        queryKey: ['created-games', loginInfo?.id],
+      });
+      queryClient.invalidateQueries({
+        queryKey: ['confirmed-games', loginInfo?.id],
+      });
+      queryClient.invalidateQueries({
+        queryKey: ['game-registration', loginInfo?.id, gameId],
+      });
+    },
   });
 };
