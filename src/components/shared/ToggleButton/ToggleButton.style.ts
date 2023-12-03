1d865b598aa2fecf2dad4d57a4acd575e1d74185
김민재 <kimminje7267@naver.com>
2023-12-03 18:07:11 +0900

    fix: 토글 버튼 스타일 수정

--- src/components/shared/ToggleButton/ToggleButton.style.ts
+++ src/components/shared/ToggleButton/ToggleButton.style.ts
@@ -7,6 +7,9 @@ export const StyledToggleButton = styled('button')<{
   fontSize: string;
   border?: string;
 }>(({ width, height, fontSize, theme, isActive, border }) => ({
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
   backgroundColor: 'white',
   width: width,
   height: height,
