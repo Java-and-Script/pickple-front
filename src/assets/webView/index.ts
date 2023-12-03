939e9b564eddd3a0349f492a3dbb604bc7931a99
Chan Lee <120288440+1eecan@users.noreply.github.com>
2023-12-03 17:12:14 +0900

    fix: 웹뷰 폴더 구조 변경 및 import 방식 수정 및 오타 수정
    *ElasticCache -> ElastiCache

---
+++ src/assets/webView/index.ts
@@ -0,0 +1,30 @@
+export { default as chatSample } from './sample/chatSample.png';
+export { default as homeSample } from './sample/homeSample.png';
+export { default as mapSample } from './sample/mapSample.png';
+
+export { default as awsEc2 } from './techStack/back/awsEc2.png';
+export { default as awsElastiCache } from './techStack/back/awsElastiCache.png';
+export { default as awsRds } from './techStack/back/awsRds.png';
+export { default as docker } from './techStack/back/docker.png';
+export { default as githubActions } from './techStack/back/githubActions.png';
+export { default as java17 } from './techStack/back/java17.png';
+export { default as jpa } from './techStack/back/jpa.png';
+export { default as mySql } from './techStack/back/mySql.png';
+export { default as nginx } from './techStack/back/nginx.png';
+export { default as redis } from './techStack/back/redis.png';
+export { default as springBoot } from './techStack/back/springBoot.png';
+
+export { default as axios } from './techStack/front/axios.png';
+export { default as emotion } from './techStack/front/emotion.png';
+export { default as eslint } from './techStack/front/eslint.png';
+export { default as figma } from './techStack/front/figma.png';
+export { default as npm } from './techStack/front/npm.png';
+export { default as prettier } from './techStack/front/prettier.png';
+export { default as react } from './techStack/front/react.png';
+export { default as reactHookForm } from './techStack/front/reactHookForm.png';
+export { default as reactRouterDom } from './techStack/front/reactRouterDom.svg';
+export { default as tanstackQuery } from './techStack/front/tanstackQuery.png';
+export { default as typeScript } from './techStack/front/typeScript.png';
+export { default as vercel } from './techStack/front/vercel.png';
+export { default as vite } from './techStack/front/vite.png';
+export { default as zustand } from './techStack/front/zustand.png';
