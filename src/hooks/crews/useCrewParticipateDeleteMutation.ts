16ba28603ca63a9218628626a574a0ec61d6e92e
이진욱 <ff981113@hanyang.ac.kr>
2023-12-03 18:49:33 +0900

    크루, 게스트 모집 참여 취소하기 api 연결 (#461)
    * feat: 크루 가입 신청 취소하기 기능 추가
    
    * feat: 게스트 모집 참여 취소하기 기능 추가

--- src/hooks/crews/useCrewParticipateDeleteMutation.ts
+++ src/hooks/crews/useCrewParticipateDeleteMutation.ts
@@ -1,23 +1,23 @@
 import { useMutation } from '@tanstack/react-query';
 import { useQueryClient } from '@tanstack/react-query';
 
-import { deleteGameParticipate } from '@api/games/deleteGameParticipate';
+import { deleteCrewParticipate } from '@api/crews/deleteCrewParticipate';
 
 import { useLoginInfoStore } from '@stores/loginInfo.store';
 
-export const useGameParticipateDeleteMutation = () => {
+export const useCrewParticipateDeleteMutation = () => {
   const queryClient = useQueryClient();
   const id = useLoginInfoStore((state) => state.loginInfo?.id);
 
   return useMutation({
-    mutationFn: deleteGameParticipate,
-    onSuccess: (_, { gameId }) => {
+    mutationFn: deleteCrewParticipate,
+    onSuccess: (_, { crewId }) => {
       queryClient.invalidateQueries({
-        queryKey: ['game-detail', gameId],
+        queryKey: ['crew-detail', crewId],
       });
       id &&
         queryClient.invalidateQueries({
-          queryKey: ['game-registration', id, gameId],
+          queryKey: ['crew-registration', id, crewId],
         });
     },
   });
